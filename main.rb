55abffce0ba5a247f5f078a8beb082985820b09b
Corbin Sykes <corbin.sykes@gmail.comm>
2013-06-25 16:08:17 -0400

    links arrays

--- main.rb
+++ main.rb
@@ -5,3 +5,22 @@ require 'sinatra/reloader' if development?
 get '/' do
   erb :home
 end
+
+# get '/manual_links' do
+#   @links = ['http://funnyjunk.com',
+#             'http://gizmodo.com',
+#             'http://reddit.com',
+#             'http://grantland.com',
+#             'http://avclub.com']
+#   erb :links
+# end
+
+get '/links' do
+  link_file = File.new('links.csv', 'r')
+  @links = []
+  link_file.each do |line|
+    @links << line.split(',')
+  end
+  link_file.close
+  erb :links
+end
